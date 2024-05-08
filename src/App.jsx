6a0aa217ba02c602cde6b1bf41741f26cbe7253d
PrathamK78 <pratham1054.be21@chitkara.edu.in>
2024-05-09 00:50:23 +0530

    Final Version⏸

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,54 @@ import JobsPage from './pages/JobsPage'
 import NotFound from './pages/NotFound'
 import SingleJobPage,{jobLoader} from './pages/SingleJobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path="/" element={<MainLayout/>}>
-    <Route index element={<Homepage/>} />
-    <Route path="/jobs" element={<JobsPage/>} />
-    <Route path="/add-job" element={<AddJobPage/>} />
-    <Route path="/jobs/:id" element={<SingleJobPage/>} loader={jobLoader} />
-    <Route path="*" element={<NotFound/>} />
-  </Route>
-  )
-);
 
 const App = () => {
+  // Add a new job to the server
+  const addJob = async(newJob) => {
+    const res = await fetch('/api/jobs',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete a job from the server
+  const deleteJob = async(id) => {
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: 'DELETE',
+    });
+    return;
+  };
+
+  // Update a job on the server
+  const updateJob = async(job) => {
+    const res = await fetch(`/api/jobs/${job.id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      <Route index element={<Homepage/>} />
+      <Route path="/jobs" element={<JobsPage/>} />
+      <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path="/jobs/:id" element={<SingleJobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+      <Route path="/edit-job/:id" element={<EditJobPage updatedJobSubmit={updateJob}/>} loader={jobLoader} />
+      <Route path="*" element={<NotFound/>} />
+    </Route>
+    )
+  );
   return <RouterProvider router={router}/>
 }
 
--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,52 +1,56 @@
-import {React,useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import {useState,React} from 'react'
+import {useParams,useLoaderData,useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
 
-const AddJobPage = ({addJobSubmit}) => {
-  const [title,setTitle] = useState('');
+const EditJobPage = ({updatedJobSubmit}) => {
+    const job = useLoaderData();
+  const [title,setTitle] = useState(job.title);
   const [type,setType] = useState('');
-  const [location,setLocation] = useState('');
-  const [description,setDescription] = useState('');
-  const [salary,setSalary] = useState('');
-  const [companyName,setCompanyName] = useState('');
-  const [companyDescription,setCompanyDescription] = useState('');
-  const [contactEmail,setContactEmail] = useState('');
-  const [contactPhone,setContactPhone] = useState('');
+  const [location,setLocation] = useState(job.location);
+  const [description,setDescription] = useState(job.description);
+  const [salary,setSalary] = useState(job.salary);
+  const [companyName,setCompanyName] = useState(job.company.name);
+  const [companyDescription,setCompanyDescription] = useState(job.company.description);
+  const [contactEmail,setContactEmail] = useState(job.company.contactEmail);
+  const [contactPhone,setContactPhone] = useState(job.company.contactPhone);
+
   const navigate = useNavigate();
 
-  const submitForm = function(e) {
-    e.preventDefault();
-    
-    // Create a new job object to send to the server
-    const newJob = {
-      title,
-      type,
-      location,
-      description,
-      salary,
-      company:{
-        name: companyName,
-        description: companyDescription,
-        contactEmail,
-        contactPhone,
+  const {id} = useParams();
+
+    const submitForm = function(e) {
+      e.preventDefault();
+      
+      // Create a new job object to send to the server
+      const updatedJob = {
+        id,
+        title,
+        type,
+        location,
+        description,
+        salary,
+        company:{
+          name: companyName,
+          description: companyDescription,
+          contactEmail,
+          contactPhone,
+        }
       }
-    }
-    addJobSubmit(newJob);
-
-    toast.success('Job added successfully ✅');
-
-    return navigate('/jobs');
-  };
-
+      updatedJobSubmit(updatedJob);
+  
+      toast.success('Job updated successfully 🤩');
+  
+      return navigate(`/jobs/${id}`);
+    };
+    
   return (
-    <>
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
           <form onSubmit={submitForm}>
-            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
@@ -211,15 +215,14 @@ const AddJobPage = ({addJobSubmit}) => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Update Job
               </button>
             </div>
           </form>
         </div>
       </div>
     </section>
-    </>
   )
 }
 
-export default AddJobPage
+export default EditJobPage
